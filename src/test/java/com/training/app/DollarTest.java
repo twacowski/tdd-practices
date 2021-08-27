81dcc7fff6b1315d7df8bb8c87744e4ea0451cef
Tomek <wacowski.tomasz@gmail.com>
2021-08-27 23:33:45 +0200

    add equals method

--- src/test/java/com/training/app/DollarTest.java
+++ src/test/java/com/training/app/DollarTest.java
@@ -2,7 +2,7 @@ package com.training.app;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class DollarTest {
 
@@ -13,5 +13,11 @@ public class DollarTest {
         assertEquals(10, product.amount);
         product = five.times(3);
         assertEquals(15, product.amount);
+    }
+
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 }
