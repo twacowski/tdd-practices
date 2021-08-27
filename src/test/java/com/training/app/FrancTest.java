4ebdaaf4737ab9c44b42ad21869a2c8b4e3b6fc1
Tomek <wacowski.tomasz@gmail.com>
2021-08-27 23:58:12 +0200

    add Franc class

---
+++ src/test/java/com/training/app/FrancTest.java
@@ -0,0 +1,21 @@
+package com.training.app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FrancTest {
+
+    @Test
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
+    }
+
+    @Test
+    public void testFrancEquality() {
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
+    }
+}
