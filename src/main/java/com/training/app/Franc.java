cc2868d38442c6b286e0f25198463f53735f0183
Tomek <wacowski.tomasz@gmail.com>
2021-08-28 09:22:02 +0200

    add Money superclass

--- src/main/java/com/training/app/Franc.java
+++ src/main/java/com/training/app/Franc.java
@@ -1,8 +1,6 @@
 package com.training.app;
 
-public class Franc {
-
-    private int amount;
+public class Franc extends Money {
 
     public Franc(int amount) {
         this.amount = amount;
@@ -10,11 +8,6 @@ public class Franc {
 
     public Franc times(int multiplier) {
         return new Franc(amount * multiplier);
-    }
-
-    public boolean equals(Object object) {
-        Franc franc = (Franc) object;
-        return amount == franc.amount;
     }
 
 }
